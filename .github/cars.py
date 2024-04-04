5292ab4aa3976a01b9e531634a993a776a40597c
efrain ignacio <efrainignacio23@gmail.com>
2024-04-03 18:19:25 -0600

    crazy

--- .github/cars.py
+++ .github/cars.py
@@ -27,11 +27,12 @@ cars.sort()
 # `problem 13. print the array to the console`
 print(cars)
 # `problem 14. Sort the array in descending order`
-
+cars.sort(reverse=True)
 # `problem 15. print the array to the console`
-
+print(cars)
 # `problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
-
+my_array_length = len(cars)
 # `problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
-
+array_string = 'The length of my array is'
 # `problem 18. print array_string concatenated with my_array_length to the console.`
+print(array_string + str(my_array_length))
