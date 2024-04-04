fc4cdcecac1116436ada09e6e788db312a79f0b9
efrain ignacio <efrainignacio23@gmail.com>
2024-04-03 18:00:41 -0600

    saver

--- .github/cars.py
+++ .github/cars.py
@@ -1,35 +1,35 @@
-`problem 1. print your first and last name` # type: ignore
-print("Efrain Ignacio")
-`problem 2. In the cars.py create an array named 'cars' with the following elements in this order  ---- Ford,Chrysler,Dodge,Ram,Jeep,Chevy,GMC` (use single quotes for each element)EX: 'Ford' not "Ford" spelling matters
+# `problem 1. print your first and last name` # type: ignore
+print('Efrain Ignacio')
+# `problem 2. In the cars.py create an array named 'cars' with the following elements in this order  ---- Ford,Chrysler,Dodge,Ram,Jeep,Chevy,GMC` (use single quotes for each element)EX: 'Ford' not "Ford" spelling matters
 cars = ['Ford', 'Chrysler', 'Dodge', 'Ram,' 'Jeep', 'Chevy', 'GMC']
-`problem 3. print the array to the console`
+# `problem 3. print the array to the console`
+print(cars)
+# `problem 4. print the length of the array to the console `
+print(lens(cars))
+# ' problem 5. Append Buick to the Array`
 
-`problem 4. print the length of the array to the console `
+# `problem 6. print the array to the console`
 
-`problem 5. Append Buick to the Array`
+# `problem 7. Print the 4th element in the array (Not index 4, element 4)`
 
-`problem 6. print the array to the console`
+# `problem 8. Insert 'Toyota' into element 3 in the array`
 
-`problem 7. Print the 4th element in the array (Not index 4, element 4)`
+# `problem 9. print the array to the console`
 
-`problem 8. Insert 'Toyota' into element 3 in the array`
+# `problem 10. Remove element 5 of the array (hint look at options for pop())`
 
-`problem 9. print the array to the console`
+# `problem 11. print the array to the console`
 
-`problem 10. Remove element 5 of the array (hint look at options for pop())`
+# `problem 12. Sort the array in ascending order`
 
-`problem 11. print the array to the console`
+# `problem 13. print the array to the console`
 
-`problem 12. Sort the array in ascending order`
+# `problem 14. Sort the array in descending order`
 
-`problem 13. print the array to the console`
+# `problem 15. print the array to the console`
 
-`problem 14. Sort the array in descending order`
+# `problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
 
-`problem 15. print the array to the console`
+# `problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
 
-`problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
-
-`problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
-
-`problem 18. print array_string concatenated with my_array_length to the console.`
+# `problem 18. print array_string concatenated with my_array_length to the console.`
